0f0f9b5c30aabd8b78c7b3d9e6ad720738c9b08c
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2026-05-09 16:45:41 -0300

    Add script to download any ROM

---
+++ downloadROM.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 1. Ask for Host, Port, User, and Password all at once (separated by space)
+echo "Enter HOST, PORT, USERNAME and PASSWORD (separated by space):"
+read -p "> " SSH_HOST SSH_PORT SSH_USER SSH_PASS
+
+echo -e "\n----------------------------------------"
+
+echo "Enter ROM name:"
+read -p "> " ROM_NAME
+
+echo -e "\n----------------------------------------"
+
+# Hardcoded list of device folders from your image
+DEVICES=("cupid" "diting" "mayfly" "thor" "unicorn" "zeus")
+IMG_FILES=("boot" "dtbo" "recovery" "vendor_boot")
+
+# Path definitions
+REMOTE_PATH="/home/joshaby/axionaosp/out/target/product"  
+LOCAL_PATH="$HOME/ROMs"                 
+
+echo "Starting download..."
+
+for DEVICE in "${DEVICES[@]}"; do
+
+    # Create the local directory for this device
+    LOCAL_DEST="$LOCAL_PATH/$DEVICE"
+    mkdir -p "$LOCAL_DEST"
+
+    # Variable with base device remote dir
+    REMOTE_DIR="$REMOTE_PATH/$DEVICE"
+
+    # Download IMG files
+    for IMG_FILE in "${IMG_FILES[@]}"; do
+        echo "----------------------------------------"
+        echo "Downloading content from device $DEVICE - IMG File $IMG_FILE.img"
+        echo "From: $REMOTE_DIR"
+        echo "To:   $LOCAL_DEST"
+        echo "----------------------------------------"
+
+        # Download IMG file
+        sshpass -p "$SSH_PASS" scp -P "$SSH_PORT" -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HOST:$REMOTE_DIR/$IMG_FILE.img" "$LOCAL_DEST/"
+    done
+
+    # Download ROM file 
+    sshpass -p "$SSH_PASS" scp -P "$SSH_PORT" -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HOST:$REMOTE_DIR/$ROM_NAME*$DEVICE*.zip" "$LOCAL_DEST/"
+done
